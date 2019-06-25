2c4658b2f70273d0f5ecae7261e321f2c0c58c7a
Gustavo <gustavoabatistela@gmail.com>
2019-06-25 16:56:14 -0300

    Work on Analysis class

--- headers/Analysis.h
+++ headers/Analysis.h
@@ -17,39 +17,39 @@ class CompMesh;
 
 class PostProcess;
 
-class Analysis
-{
+class Analysis {
+
 protected:
     CompMesh *cmesh;
-    
+
     Matrix Solution;
-    
+
     Matrix GlobalSystem;
-    
+
     Matrix RightHandSide;
-    
+
 public:
-    
+
     Analysis();
-    
+
     Analysis(const Analysis &cp);
-    
+
     Analysis &operator=(const Analysis &cp);
-    
+
     ~Analysis();
-    
+
     Analysis(CompMesh *cmesh);
-    
+
     void SetMesh(CompMesh *cmesh);
-    
+
     CompMesh *Mesh() const;
-    
+
     void RunSimulation();
-    
+
     void PostProcessSolution(const std::string &filename, PostProcess &defPostProc) const;
-    
+
     VecDouble PostProcessError(std::ostream &out, PostProcess &defPostProc) const;
-    
+
 };
 
 #endif /* Analysis_h */
--- src/Analysis.cpp
+++ src/Analysis.cpp
@@ -4,45 +4,82 @@
  * and open the template in the editor.
  */
 
+#include <GeoMesh.h>
+#include <Assemble.h>
+#include <VTKGeoMesh.h>
 #include "Analysis.h"
 #include "tpanic.h"
 
 using namespace std;
 
-Analysis::Analysis() {
-    DebugStop();
+Analysis::Analysis() : cmesh(nullptr), Solution(), GlobalSystem(), RightHandSide() {
+}
 
+Analysis::Analysis(CompMesh *mesh) : Solution(), GlobalSystem(), RightHandSide() {
+    SetMesh(mesh);
 }
 
 Analysis::Analysis(const Analysis &cp) {
-    DebugStop();
+    if (&cp != this) {
+        cmesh = cp.cmesh;
+        Solution = cp.Solution;
+        GlobalSystem = cp.GlobalSystem;
+        RightHandSide = cp.RightHandSide;
+    }
 }
 
 Analysis &Analysis::operator=(const Analysis &cp) {
-    DebugStop();
+    if (&cp != this) {
+        cmesh = cp.cmesh;
+        Solution = cp.Solution;
+        GlobalSystem = cp.GlobalSystem;
+        RightHandSide = cp.RightHandSide;
+    }
+    return *this;
 }
 
-Analysis::~Analysis() {
-}
-
-Analysis::Analysis(CompMesh *mesh) {
-    DebugStop();
-}
+Analysis::~Analysis() = default;
 
 void Analysis::SetMesh(CompMesh *mesh) {
-    DebugStop();
+
+    if (mesh == nullptr) DebugStop();
+    cmesh = mesh;
+
 }
 
 CompMesh *Analysis::Mesh() const {
-    DebugStop();
+    return cmesh;
 }
 
 void Analysis::RunSimulation() {
-    DebugStop();
+
+    Assemble assemble(cmesh);
+    int neq = assemble.NEquations();
+
+    GlobalSystem.Resize(neq, neq);
+    GlobalSystem.Zero();
+
+    RightHandSide.Resize(neq, 1);
+    RightHandSide.Zero();
+
+    Solution.Resize(neq, 1);
+    Solution.Zero();
+
+    assemble.Compute(GlobalSystem, RightHandSide);
+    GlobalSystem.Solve_LU(RightHandSide);
+
+    VecDouble sol(neq, 0.);
+
+    for (int64_t ieq = 0; ieq < neq; ieq++) {
+        sol[ieq] = RightHandSide(ieq, 0);
+        Solution(ieq, 0) = RightHandSide(ieq, 0);
+    }
+
+    cmesh->LoadSolution(sol);
 }
 
 void Analysis::PostProcessSolution(const std::string &filename, PostProcess &defPostProc) const {
-    DebugStop();
+    VTKGeoMesh::PrintSolVTK(cmesh, defPostProc, filename);
 }
 
 VecDouble Analysis::PostProcessError(std::ostream &out, PostProcess &defPostProc) const {
